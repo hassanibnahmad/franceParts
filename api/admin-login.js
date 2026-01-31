620d7b211a686be785ac7a2e591962c0d7809ae5
hassanibnahmad <hassanibnahamd6@gmail.com>
2026-01-31 16:12:23 +0100

    Add maintenance page with warning message

--- api/admin-login.js
+++
@@ -1,7 +0,0 @@
-import adminHandler from './admin.js';
-
-export default async function handler(req, res) {
-  // Delegate to consolidated admin handler which implements login
-  // behaviour for paths including /admin-login, /admin-token, etc.
-  return adminHandler(req, res);
-}
